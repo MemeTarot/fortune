6574abfadae43d4b6562b8745f438c9f137deac7
Silverhawk <sondra.silverhawk@hush.com>
2018-11-11 11:44:52 -0800

    Add models for major and minor arcana and import to admin

--- reader/admin.py
+++ reader/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import MajorArcanaCard
+from .models import MinorArcanaCard
 
-# Register your models here.
+# Register models
+admin.site.register(MajorArcanaCard)
+admin.site.register(MinorArcanaCard)
