d11f4696806767fc08ba790650505d4149ef03a7
Silverhawk <sondra.silverhawk@hush.com>
2018-11-10 17:00:12 -0800

    Add model for cards

--- reader/models.py
+++ reader/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Card(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
+    meaning = models.TextField()
+    imgUrl = models.TextField()
 
     def __str__(self):
         return self.title
