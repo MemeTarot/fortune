eb7d5f6f37b2ca3a64a47ea088a9565c00a20e5f
Silverhawk <sondra.silverhawk@hush.com>
2018-11-12 12:43:02 -0800

    Add dynamic view to show all major arcana cards title and image

--- reader/views.py
+++ reader/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import MajorArcanaCard
 
 # Create your views here.
+def card_list(request):
+    cards = MajorArcanaCard.objects.all()
+    return render(request, 'reader/card_list.html', {'cards': cards})
+
