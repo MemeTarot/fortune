701718a5c3718055272d38027c5e196b3e199b23
kuma <karhu0009@gmail.com>
2018-11-28 04:57:38 +0000

    Update file formatting.

--- reader/views.py
+++ reader/views.py
@@ -3,15 +3,13 @@ from .models import MajorArcanaCard
 from random import sample
 
 
-# Create your views here.
 def home(request):
     return render(request, 'reader/home.html')
 
 
 def readings(request):
-    cards = MajorArcanaCard.objects.order_by('number')
+    cards = MajorArcanaCard.objects.all()
     num_cards = int(request.POST.get('spread'))
-    print(num_cards)
     spread = choose_items(cards, num_cards)
     return render(request, 'reader/readings.html', {'spread': spread})
 
