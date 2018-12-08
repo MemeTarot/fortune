cea2160e5147d574652f4642449914eb22b40dcc
kuma <karhu0009@gmail.com>
2018-12-08 01:36:06 +0000

    Readings now show appropriate card placements and associated information.

--- reader/views.py
+++ reader/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.contrib.staticfiles.templatetags.staticfiles import static
 from .forms import SpreadForm
+from .models import MajorArcanaCard, Placement
 from random import sample
-from .models import MajorArcanaCard
+
 
 # Create your views here.
 
@@ -11,20 +12,22 @@ def home(request):
         form = SpreadForm(request.GET)
     else:
         form = SpreadForm()
+
     return render(request, 'reader/home.html', {'form': form})
+
 
 def readings(request):
     cards = MajorArcanaCard.objects.all()
-
     for card in cards:
         card.url = url = static(card.imgUrl)
 
-    spread = request.POST.get('spread')
-
+    spread_pk = int(request.POST.get('spread'))
+    placements = [place for place in Placement.objects.all() if place.spread.pk == spread_pk]
     random_cards = choose_items(cards, 3)
+    reading = zip(placements, random_cards)
 
+    return render(request, 'reader/readings.html', {'reading': reading})
 
-    return render(request, 'reader/readings.html', {'spread': random_cards})
 
 def choose_items(collection, n):
     """Returns a list of <n> random items from <collection>."""
