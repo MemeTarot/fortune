ed7c196e1279a9e1d906d10555f8dca53070fd96
kuma <karhu0009@gmail.com>
2018-12-08 00:00:05 +0000

    Dropdown menu for Spreads. Propagated changes to views

---
+++ reader/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Spread
+
+
+class SpreadForm(forms.Form):
+    SPREAD_CHOICES = [(spread, spread.title) for spread in Spread.objects.all()]
+
+    spread = forms.CharField(label='Choose a spread',
+                             widget=forms.Select(choices=SPREAD_CHOICES))
+
+
+
